443c51bbe98664fb93beb167756e8c3ce650a55a
Sobia <sobiacodes@gmail.com>
2023-10-19 21:12:16 +0100

    test posts added and async funcs used to get users and posts then displayed them in the console to see if I can access the data

--- src/app/components/Posts.tsx
+++ src/app/components/Posts.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 
+// prisma client
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const Posts = async () => {
+    // users
+    const users = await prisma.user.findMany();
+    console.log(users);
+ 
 
 
-const Posts = () => {
+    // posts
+    const posts = await prisma.post.findMany();
+    console.log(posts);
+    
+
+
+
     return (
         <div>
             <p>Posts component</p>
+
+
         </div>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import MainSection from "./components/MainSection";
 
+// db
+import Posts from "./components/Posts";
+
 export default function Home() {
   return (
     <main>
       {/* <h1>Home main component</h1> */}
       <MainSection />
+      <Posts />
+
     </main>
   )
 }
