8ad9bfc75935e77c8ad72fc9edd2ba8278f5db32
Sobia <sobiacodes@gmail.com>
2023-10-19 19:40:26 +0100

    footer done

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="py-12 backdrop-blur-lg border-t border-cyan-400 my-12">
+            <div className="container mx-auto text-center">
+                <a href="https://github.com/sobiaahmad94">
+                    <div className="text-[10px]">
+                        Copyright &copy; Sobiverse Blog 2023. Created and written by Sobia Ahmad, whilst drinking lots of chai ☕
+                    </div>
+                </a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
