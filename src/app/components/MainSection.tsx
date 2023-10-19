16f89289ee88e1305b3c4f3b049fae4eb92f9b2d
Sobia <sobiacodes@gmail.com>
2023-10-19 20:28:20 +0100

    main section added and prisma/next-auth installed

--- src/app/components/MainSection.tsx
+++ src/app/components/MainSection.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const MainSection = () => {
     return (
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-12">
             <p>MainSection component</p>
-            <button>Browse Blog Articles</button>
+            <button className="bg-gray-600">Browse Blog Articles</button>
 
         </div>
     );
